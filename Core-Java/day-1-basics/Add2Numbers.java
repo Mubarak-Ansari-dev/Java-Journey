254f7ac0b914defb0151c1cd9829e65c2cf81f6c
Mubarak-Ansari <mubarakansari8948@gmail.com>
2026-04-02 19:56:58 +0530

    Add program: Add two numbers

---
+++ Core-Java/day-1-basics/Add2Numbers.java
@@ -0,0 +1,12 @@
+// This program adds two numbers and prints the result
+
+public class Add2Numbers {
+    public static void main(String args[]) {
+        int num1 = 7;
+        int num2 = 9;
+
+        int sum = num1 + num2;
+
+        System.out.println(sum);
+    }
+}
