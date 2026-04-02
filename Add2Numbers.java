ec1575cffc0ce0df84df0291388eaba6b0d69271
Mubarak-Ansari <mubarakansari8948@gmail.com>
2026-04-02 19:44:18 +0530

    Add program: Add two Numbers

---
+++ Add2Numbers.java
@@ -0,0 +1,12 @@
+
+
+public class Add2Numbers {
+    public static void main(String args[]) {
+        int num1 = 7;
+        int num2 = 9;
+
+        int sum = num1 + num2;
+
+        System.out.println(sum);
+    }
+}
